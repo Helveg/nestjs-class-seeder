6ded0d46dc27a380d58f5d932e252f95adfa4656
Robin De Schepper <robin.deschepper93@gmail.com>
2022-08-23 10:22:12 +0200

    Export SeederService

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 export * from './seeder/seeder';
 export * from './seeder/seeder.interface';
+export * from './seeder/seeder.service';
 export * from './factory/data.factory';
 export * from './decorators/factory.decorator';
