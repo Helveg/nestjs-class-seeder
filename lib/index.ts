06e898ca1d7fb65a4e5d9d20da6dab55b36e9f79
Robin De Schepper <robin.deschepper93@gmail.com>
2022-10-22 20:05:55 +0200

    export class seeder

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@ export * from './seeder/seeder';
 export * from './seeder/seeder.interface';
 export * from './seeder/seeder.service';
 export * from './decorators/seed.decorator';
+export * from './seeders/class-seeder';
