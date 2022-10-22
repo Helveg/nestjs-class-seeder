6ca393290c51c54f05769f4916e9da236b906210
Robin De Schepper <robin.deschepper93@gmail.com>
2022-10-22 20:06:08 +0200

    forgot a few ducks

--- sample/src/app.module.ts
+++ sample/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Duck } from './duck.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: './ducks.db',
+    entities: [Duck]
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
