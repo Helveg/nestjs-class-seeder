d4b305297d270739f0f85d9b753b2090d604ea19
Robin De Schepper <robin.deschepper93@gmail.com>
2022-10-23 19:05:50 +0200

    Import all decorators from barrel file

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 export * from './seeder/seeder';
 export * from './seeder/seeder.interface';
 export * from './seeder/seeder.service';
-export * from './decorators/seed.decorator';
 export * from './seeders/class-seeder';
+export * from './decorators';
 export * from './interfaces';
