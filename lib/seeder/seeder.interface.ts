e4e02759c83c9f8ddd75d95ff88a61dd4ce76ac1
Robin De Schepper <robin.deschepper93@gmail.com>
2022-10-26 18:05:07 +0200

    Added `getName` to seeder interface

--- lib/seeder/seeder.interface.ts
+++ lib/seeder/seeder.interface.ts
@@ -1,6 +1,7 @@
 import { DropContext, SeederContext } from "../interfaces/context.interface";
 
 export interface Seeder {
+  getName(): string;
   seed(context: SeederContext): Promise<any>;
   drop(context: DropContext): Promise<any>;
 }
