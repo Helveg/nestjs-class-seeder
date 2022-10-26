2d2e73754b1bcc28689e14a8ea868beb892e0848
Robin De Schepper <robin.deschepper93@gmail.com>
2022-10-26 18:05:33 +0200

    Added ClassRef, Ref, and ForwardRef

---
+++ lib/relationships/references.ts
@@ -0,0 +1,34 @@
+import { Type } from "@nestjs/common";
+import { SeedRelationQuery } from "../interfaces/seed-relation.interface";
+import { SeederContext } from "../interfaces/context.interface";
+import { pickRelated } from "./pick";
+
+export type ClassRef<T> = () => new () => T;
+
+export abstract class Ref {
+  public selfClass: Type<any>;
+  public selfId: number;
+
+  constructor(
+    public propertyKey: string | symbol,
+    public context: SeederContext,
+    public refClass: Type<any>,
+    public pick: SeedRelationQuery,
+    public options: any = {}
+  ) {
+    this.selfClass = context.currentClass;
+    this.selfId = context.currentIndex;
+  }
+
+  abstract resolve(entities: any[]): Promise<any>
+}
+
+export class ForwardRef<T = any> extends Ref {
+  async resolve(entities: T[]) {
+    const self = this.context.savedEntities.get(this.selfClass)[this.selfId];
+    const idColumns = this.context.dataSource.getMetadata(this.refClass).primaryColumns
+      .map(col => col.propertyName);
+    self[this.propertyKey] = await pickRelated(idColumns, entities, this.pick);
+    return this.context.dataSource.getRepository(this.selfClass).save(self);
+  }
+}
